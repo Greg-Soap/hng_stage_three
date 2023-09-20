c9693c67cec52ba04638592cc0ce8208a40e56d3
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-09-20 15:43:17 +0100

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,65 +3,51 @@ import { Reorder, useDragControls, useMotionValue } from "framer-motion";
 import { useState } from "react";
 import { useRaisedShadow } from "@/utils/useRaisedShadow";
 import { ReorderIcon } from "@/components/dragIcon";
-import AuthForm from "./auth-form";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClientComponentClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   const [items, setItems] = useState(posts);
   return (
     <div className="App">
-      <div className="row">
-        <div className="col-9">
-          <h1 className="header">Supabase Auth + Storage</h1>
-          <p className="">
-            Experience our Auth and Storage through a simple profile management
-            example. Create a user profile and upload an avatar image. Fast,
-            simple, secure.
-          </p>
-        </div>
-        <div className="col-3 auth-widget">
-          {/* <AuthForm /> */}
-          <button>
-            {" "}
-            <a href="/login">Login</a>
-          </button>
-        </div>
-      </div>
-
-      <div className="layout">
-        <h3>List of stuff</h3>
-        <Reorder.Group
-          axis="y"
-          onReorder={setItems}
-          values={items}
-          className="card_wrapper"
-        >
-          {items.map((item) => (
-            <Item key={item} item={item} drag />
-          ))}
-        </Reorder.Group>
-      </div>
+      <Reorder.Group
+        axis="y"
+        onReorder={setItems}
+        values={items}
+        className="card_wrapper"
+      >
+        {items.map((item) => (
+          <Item key={item} item={item} user={user} drag />
+        ))}
+      </Reorder.Group>
     </div>
   );
 }
-function Item({ item }: any) {
+function Item({ item, user }: any) {
   const y = useMotionValue(0);
   const boxShadow = useRaisedShadow(y);
   const dragControls = useDragControls();
+
   return (
     <Reorder.Item
       value={item}
       id={item}
       style={{ boxShadow, y }}
       dragListener={false}
-      dragControls={dragControls}
+      dragControls={user && dragControls}
       className="card"
       drag
     >
       <h4>List Item {item}</h4>
       <p>this is inside the card</p>
-      <span className="dragger">
-        <ReorderIcon dragControls={dragControls} />
-      </span>
+      {user && (
+        <span className="dragger">
+          <ReorderIcon dragControls={dragControls} />
+        </span>
+      )}
     </Reorder.Item>
   );
 }
